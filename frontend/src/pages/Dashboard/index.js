d2f851052808b9999c541b06bc0b8048e9b4efad
Bruno Facundo <brunofr95@gmail.com>
2019-09-12 15:41:44 -0300

    Add page dashboard.

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,6 +1,57 @@
-import React from 'react';
-import { Container } from './styles';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import React, { useEffect, useState } from 'react';
+import { MdAddCircleOutline, MdKeyboardArrowRight } from 'react-icons/md';
+import api from '~/services/api';
+import history from '~/services/history';
+import { Container, Header, Meetups } from './styles';
 
 export default function Dashboard() {
-    return <Container />;
+    const [meetups, setMeetups] = useState([]);
+
+    useEffect(() => {
+        async function loadMeetups() {
+            const response = await api.get('organizing');
+            const data = response.data.map(meetup => ({
+                ...meetup,
+                dateFormatted: format(parseISO(meetup.date), "d 'de' MMMM', às ' H'h'", { locale: pt })
+            }));
+
+            setMeetups(data);
+        }
+
+        loadMeetups();
+    }, []);
+
+    function handlNewMeetup() {
+        history.push('/meetup');
+    }
+
+    function handleMeetupDetail(meetup) {
+        history.push('/meetup', { meetup });
+    }
+
+    return (
+        <Container>
+            <Header>
+                <strong>Meus meetups</strong>
+                <button type="button" onClick={handlNewMeetup}>
+                    <MdAddCircleOutline color="#fff" size={24} />
+                    Novo meetup
+                </button>
+            </Header>
+
+            <Meetups>
+                {meetups.map(meetup => (
+                    <li key={meetup.id} onClick={() => handleMeetupDetail(meetup)}>
+                        <strong>{meetup.title}</strong>
+                        <div>
+                            <span> {meetup.dateFormatted}</span>
+                            <MdKeyboardArrowRight color="#fff" size={24} />
+                        </div>
+                    </li>
+                ))}
+            </Meetups>
+        </Container>
+    );
 }
--- frontend/src/pages/Dashboard/styles.js
+++ frontend/src/pages/Dashboard/styles.js
@@ -1,6 +1,80 @@
+import { darken } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    max-width: 600px;
+    max-width: 950px;
     margin: 50px auto;
+    padding: 0 25px;
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 30px;
+
+    strong {
+        color: #fff;
+        font-size: 18px;
+    }
+
+    button {
+        display: flex;
+        align-items: center;
+        background: #f94d6a;
+        font-weight: bold;
+        color: #fff;
+        border: 0;
+        border-radius: 4px;
+        font-size: 16px;
+        transition: background 0.2s;
+        width: fit-content;
+        padding: 10px 20px;
+
+        &:hover {
+            background: ${darken(0.03, '#f94d6a')};
+        }
+
+        svg {
+            margin-right: 5px;
+        }
+    }
+`;
+
+export const Meetups = styled.ul`
+    li {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background: rgba(0, 0, 0, 0.2);
+        padding: 20px 30px;
+        border-radius: 4px;
+        cursor: pointer;
+        transition: background 0.2s;
+
+        &:hover {
+            background: rgba(0, 0, 0, 0.3);
+        }
+
+        strong {
+            color: #fff;
+        }
+
+        div {
+            display: flex;
+            align-items: center;
+
+            span {
+                color: rgba(255, 255, 255, 0.6);
+            }
+
+            svg {
+                margin-left: 40px;
+            }
+        }
+    }
+
+    li + li {
+        margin-top: 20px;
+    }
 `;
