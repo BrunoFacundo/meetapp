ef48d156626bc6d978fa2001c63fe717594faf89
Bruno Facundo <brunofr95@gmail.com>
2019-09-12 02:17:38 -0300

    Add page Profile

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 import Route from './Route';
@@ -12,6 +13,7 @@ export default function Routes() {
             <Route path="/register" component={SignUp} />
 
             <Route path="/dashboard" component={Dashboard} isPrivate />
+            <Route path="/profile" component={Profile} isPrivate />
         </Switch>
     );
 }
