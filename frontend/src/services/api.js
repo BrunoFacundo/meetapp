8f0a7520c355cb2245a1637a2aa4d34f4a146e59
Bruno Facundo <brunofr95@gmail.com>
2019-09-23 13:36:41 -0300

    Fix ip in api service.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : 'http://35.211.143.59'
 });
 
 export default api;
