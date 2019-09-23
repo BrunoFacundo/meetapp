7750a4bdb31b5d8f423aaa69b0fc78c9cea9036a
Bruno Facundo <brunofr95@gmail.com>
2019-09-23 15:35:02 -0300

    Update ip backend.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : 'http://35.211.143.59'
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : 'http://34.67.211.35'
 });
 
 export default api;
