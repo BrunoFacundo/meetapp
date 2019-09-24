3e9f53d060614e933c441cb946a7434b2dc5d0f8
Bruno Facundo <brunofr95@gmail.com>
2019-09-24 11:10:10 -0300

    Update url server api.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : 'http://34.67.211.35'
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : 'https://api.meetapp-bootcamp.tk'
 });
 
 export default api;
