b2a2feef3c2033d3ed5c0f7eabcbe3d3b8fa654e
Bruno Facundo <brunofr95@gmail.com>
2019-10-01 19:01:10 -0300

    Update seed.

--- backend/src/database/seeds/20190930224153-users.js
+++ backend/src/database/seeds/20190930224153-users.js
@@ -2,29 +2,28 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
     up: async queryInterface => {
-        return queryInterface.bulkInsert(
-            'users',
-            [
-                {
-                    name: 'Rocketseat Experience',
-                    email: 'rsxp@rocketseat.com.br',
-                    password_hash: await bcrypt.hash('12345678', 8),
-                    created_at: new Date(),
-                    updated_at: new Date()
-                },
-                {
-                    name: 'Bruno Facundo',
-                    email: 'brunofr95@gmail.com',
-                    password_hash: await bcrypt.hash('12345678', 8),
-                    created_at: new Date(),
-                    updated_at: new Date()
-                }
-            ],
-            {}
-        );
+        const users = [
+            {
+                name: 'Rocketseat Experience',
+                email: 'rsxp@rocketseat.com.br',
+                password_hash: await bcrypt.hash('12345678', 8),
+                created_at: new Date(),
+                updated_at: new Date()
+            },
+            {
+                name: 'Bruno Facundo',
+                email: 'brunofr95@gmail.com',
+                password_hash: await bcrypt.hash('12345678', 8),
+                created_at: new Date(),
+                updated_at: new Date()
+            }
+        ];
+
+        await queryInterface.bulkInsert('users', users, {});
     },
 
-    down: queryInterface => {
-        return queryInterface.bulkDelete('users', null, {});
+    down: async queryInterface => {
+        await queryInterface.bulkDelete('users', null, {});
+        await queryInterface.sequelize.query('ALTER SEQUENCE "users_id_seq" RESTART WITH 1');
     }
 };
