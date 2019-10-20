338d2ed0a7699efc68ca5b6d7b35c5753f9d4d4f
Bruno Facundo <brunofr95@gmail.com>
2019-10-19 23:38:47 -0300

    Fix time zone.

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
+import { getTimeZone } from 'react-native-localize';
 
 const api = axios.create({
-    baseURL: __DEV__ ? 'http://10.0.2.2:3333' : 'https://api.meetapp-bootcamp.tk'
+    baseURL: __DEV__ ? 'http://10.0.2.2:3333' : 'https://api.meetapp-bootcamp.tk',
+    headers: {
+        timezone: getTimeZone()
+    }
 });
 
 export default api;
